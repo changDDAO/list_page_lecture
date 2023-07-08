935149e4fe9d5896cb8fd08e0e7e114c8b5e9630
Changhogod <younch8342@naver.com>
2023-07-08 22:29:15 +0900

    listview.builder & convert page

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:list_page_lecture/model.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyPage(),
+    );
+
+  }
+
+}
+class MyPage extends StatefulWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  static List<String> animalName = [
+    'Bear',
+    'Camel',
+    'Deer',
+    'Fox',
+    'Kangaroo',
+    'Koala',
+    'Lion',
+    'Tiger',
+  ];
+  static List<String> animalImagePath = [
+    'image/bear.png',
+    'image/camel.png',
+    'image/deer.png',
+    'image/fox.png',
+    'image/kangaroo.png',
+    'image/koala.png',
+    'image/lion.png',
+    'image/tiger.png',
+
+  ];
+  static List<String> animalLocation = [
+    'forest',
+    'dessert',
+    'mountain',
+    'snow mountain',
+    'Australia',
+    'Afreeca',
+    'Europe',
+    'korea',
+  ];
+
+  final List<Animal> animalData = List.generate(
+      animalName.length, (index) =>
+      Animal(animalName[index], animalImagePath[index], animalLocation[index]));
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView'),
+      ),
+      body: ListView.builder(
+          itemCount: animalData.length,
+          itemBuilder: (context, index){
+            return Card(
+              child: ListTile(
+                title: Text(
+                animalData[index].name,
+              ),
+            leading: SizedBox(
+              height: 50, width: 50,
+              child: Image.asset(animalData[index].imgPath),
+            )),
+            );
+          }
+      ),
+    );
+  }
+}
+
+
+
